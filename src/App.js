79fa96dea2b39e77c41be74eced612abd0ba4778
selvam <selvamplkm111@gmail.com>
2022-11-17 15:15:41 +0530

    add-code.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import FormSec from "./pages/Form.js";
+// import Register from "./Test.js";
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+        <FormSec/>
+        {/* <Register/> */}
+    </div>
+  );
+}
+
+export default App;
